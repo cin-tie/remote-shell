b962bb46e3acd60fb121cd32d0b86d2d046fb56f
cin-tie <csizegod@gmail.com>
2025-11-12 09:04:15 +0300

    Directory changes and getting added

---
+++ src/csdev/MessageChdir.java
@@ -0,0 +1,20 @@
+package csdev;
+
+import java.io.Serializable;
+
+/**
+ * MessageChdir class: change working directory
+ * @author cin-tie
+ * @version 1.0
+ */
+public class MessageChdir extends Message implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public String newDirectory;     // New directory
+
+    public MessageChdir(String newDirectory) {
+        super(Protocol.CMD_CHDIR);
+        this.newDirectory = newDirectory;
+    }
+}
---
+++ src/csdev/MessageGetdir.java
@@ -0,0 +1,17 @@
+package csdev;
+
+import java.io.Serializable;
+
+/**
+ * <p>MessageGetdir class: get current working directory
+ * @author cin-tie
+ * @version 1.0
+ */
+public class MessageGetdir extends Message implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public MessageGetdir() {
+        super(Protocol.CMD_GETDIR);
+    }
+}
