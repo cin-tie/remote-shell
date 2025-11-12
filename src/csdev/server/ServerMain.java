d66de7a29fce44d535f02f7784a0cff708c4aa0f
cin-tie <csizegod@gmail.com>
2025-11-12 09:47:15 +0300

    Refactoring messages, adding utils

--- src/csdev/server/ServerMain.java
+++ src/csdev/server/ServerMain.java
@@ -1,7 +1,18 @@
 package csdev.server;
 
+import java.util.logging.Logger;
+
+/**
+ * <p>Main class of server application for remote shell
+ * <p>Realized in console
+ * @author cin-tie
+ * @version 1.0
+ */
 public class ServerMain {
 
     private static int MAX_USERS = 50;
     private static int SOCKET_TIMEOUT = 1000;
+
+    public static void main(String[] args) {
+    }
 }
