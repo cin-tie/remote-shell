cf67f806776f94292f9556144a56bb36e70be8de
cin-tie <csizegod@gmail.com>
2025-11-22 11:52:37 +0300

    Rmi client session addded

--- src/csdev/threads/RmiServerThread.java
+++ src/csdev/threads/RmiServerThread.java
@@ -1,4 +1,11 @@
 package csdev.threads;
 
-public class RmiServerThread {
+import java.rmi.registry.Registry;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RmiServerThread extends Thread implements RemoteShellService{
+
+    private Registry registry;
+    private boolean running = true;
+    private ConcurrentHashMap<String, RmiClientSession> sessions = new ConcurrentHashMap<>();
 }
