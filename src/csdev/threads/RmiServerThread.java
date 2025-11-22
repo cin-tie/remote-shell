0899325ba9cc19c4c078be442a2d76adff92d6e7
cin-tie <csizegod@gmail.com>
2025-11-22 11:59:06 +0300

    Added names for threads(not all)

--- src/csdev/threads/RmiServerThread.java
+++ src/csdev/threads/RmiServerThread.java
@@ -1,5 +1,8 @@
 package csdev.threads;
 
+import csdev.threads.session.RmiClientSession;
+
+import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,4 +11,10 @@ public class RmiServerThread extends Thread implements RemoteShellService{
     private Registry registry;
     private boolean running = true;
     private ConcurrentHashMap<String, RmiClientSession> sessions = new ConcurrentHashMap<>();
+
+    public RmiServerThread() throws RemoteException {
+        super();
+        this.setDaemon(true);
+        this.setName("RmiServerThread");
+    }
 }
